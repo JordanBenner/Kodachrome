987e806e61325586fafd7a590fb61c5a899cbb71
Jordan Benner <jordanbenner@outlook.com>
2017-08-22 13:25:47 -0500

    quantcast
    Monitor traffic for user xp

--- homepage/static/base.js
+++ homepage/static/base.js
@@ -10,3 +10,28 @@ function show_p(a, id) {
 
   p.scrollIntoView(true);
 }
+
+<!-- Quantcast Tag -->
+<script type="text/javascript">
+var _qevents = _qevents || [];
+
+(function() {
+var elem = document.createElement('script');
+elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+elem.async = true;
+elem.type = "text/javascript";
+var scpt = document.getElementsByTagName('script')[0];
+scpt.parentNode.insertBefore(elem, scpt);
+})();
+
+_qevents.push({
+qacct:"p-tAMkw8d3tEH3z"
+});
+</script>
+
+<noscript>
+<div style="display:none;">
+<img src="//pixel.quantserve.com/pixel/p-tAMkw8d3tEH3z.gif" border="0" height="1" width="1" alt="Quantcast"/>
+</div>
+</noscript>
+<!-- End Quantcast tag -->
